844756c37784783cb10b9d628a2d2dc872860ff2
dead-py <pydead@gmail.com>
2021-05-11 18:59:34 -0300

    Fixed yt command not playing audio

--- astolfo_bot.py
+++ astolfo_bot.py
@@ -2,7 +2,9 @@ import discord
 from discord.ext import commands
 from discord.utils import get
 import random
-import youtube_dl
+from youtube_dl import YoutubeDL
+from discord import FFmpegPCMAudio
+
 
 print("Starting bot ...\n")
 intents = discord.Intents.all()
@@ -158,46 +160,6 @@ Member Joined At: {member.joined_at}
                                 ''')
     print(" MEMBER_INFO END ".center(50, '-'), '\n')
 
-
-youtube_dl.utils.bug_reports_message = lambda: ''
-ytdl_format_options = {
-    'format': 'bestaudio/best',
-    'restrictfilenames': True,
-    'noplaylist': True,
-    'nocheckcertificate': True,
-    'ignoreerrors': False,
-    'logtostderr': False,
-    'quiet': True,
-    'no_warnings': True,
-    'deafault_search': 'auto',
-    'source_address': '0.0.0.0'
-}
-
-ffmpeg_options = {
-    'options': '-vn'
-}
-
-ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
-class YTDLSource(discord.PCMVolumeTransformer):
-    def __init__(self, source, *, data, volume=0.5):
-        super().__init__(source, volume)
-        self.data = data
-        self.title - data.get('title')
-        self.url = ""
-
-    @classmethod
-    async def from_url(cls, url, *, loop=None, stream=False):
-        loop = loop or asyncio.get_event_loop()
-        data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=not stream))
-        if 'entries' in data:
-
-            #takes first item from a playlist
-            data = data['entries'][0]
-
-        filename = data['title'] if stream else ytdl.prepare_filename(data)
-        return filename
-
-
 @bot.command()
 async def join(ctx):
     print(" JOIN START ".center(50, '-'), '\n')
@@ -231,18 +193,37 @@ async def leave(ctx):
 
 @bot.command()
 async def play(ctx, url):
+    print(" PLAY START ".center(50, '-'), '\n')
     try:
-        print('trying')
-        server = ctx.message.guilda
+        server = ctx.message.guild
+        print(server)
         voice_channel = server.voice_client
-
-        async with ctx.typing():
-            print('playing')
-            filename = await YTDLSource.from_url(url, loop=bot.loop)
-            voice_channel.play(discord.FFmpegPCMAudio(executable="ffmpeg.exe", source=filename))
+        print(voice_channel)
+        filename = await YTDLSource.from_url(url, loop=bot.loop, stream=True)
+        print(f'Filename - {filename}')
+        voice_client.play(filename)
+        voice_channel.play(filename)
         await ctx.send(f'** Now Playing: **\n{filename}')
 
     except:
         await ctx.send("The bot is not connected to a voice channel.")
+    print(" PLAY END ".center(50, '-'), '\n')
+
+
+@bot.command()
+async def yt(ctx, url):
+    YDL_OPTIONS = {'format': 'bestaudio', 'noplaylist':'True'}
+    FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
+    voice = get(bot.voice_clients, guild=ctx.guild)
+
+    if not voice.is_playing():
+        with YoutubeDL(YDL_OPTIONS) as ydl:
+            info = ydl.extract_info(url, download=False)
+        URL = info['formats'][0]['url']
+        voice.play(discord.FFmpegPCMAudio(URL, **FFMPEG_OPTIONS, executable="C:/ffmpeg/bin/ffmpeg.exe"))
+        voice.is_playing()
+    else:
+        await ctx.send("Already playing song")
+        return
 
 bot.run(token)
